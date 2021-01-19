7228895af2507b6a72b264006889da5c03e61fb1
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-01-19 15:35:56 +0700

    Finish Q2

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+void stat(const double data[], int n, double res[]){
+    double sum = 0;
+    double geom = 1;
+    double harmonicSum = 0;
+    double sdSum = 0;
+    double max;
+    double min;
+
+    for(int i = 0; i < n; ++i){
+        sum += data[i];
+        
+        if (i == 0){
+            max = data[i];
+            min = data[i];
+        }
+
+        if (data[i] < min) min = data[i];
+        if (data[i] > max) max = data[i];
+
+        harmonicSum += (1.0/data[i]);
+        geom *= data[i];
+    }
+
+    res[0] = sum / n;
+    for (int j = 0; j < n; ++j){
+        sdSum += pow(data[j] - res[0], 2);
+    }
+
+    res[1] = sqrt(sdSum / (n));
+    res[2] = pow(geom, (1.0/n));
+    res[3] = n / harmonicSum;
+    res[4] = max;
+    res[5] = min;
+}
